4ca8ecd8fa97b655186cd192b41a22153d375018
Tom Flídr <tomflidr@gmail.com>
2017-10-16 18:47:13 +0200

    nuget cfg updates

--- Connection.vb
+++ Connection.vb
@@ -17,21 +17,21 @@ Public Class Connection
 
     Public Overrides ReadOnly Property Statement As System.Type = GetType(Statement)
 
-    Public Overrides Sub Open(dsn As String)
-        Me._provider = New OracleConnection(dsn)
-        Me._provider.Open()
-        AddHandler Me._provider.InfoMessage, AddressOf Connection.errorHandler
-    End Sub
+	Public Overrides Sub Open(dsn As String)
+		Me._provider = New OracleConnection(dsn)
+		Me._provider.Open()
+		AddHandler Me._provider.InfoMessage, AddressOf Connection.errorHandler
+	End Sub
 
-    Protected Shared Sub errorHandler(sender As Object, args As OracleInfoMessageEventArgs)
-        Dim sqlErrors As Databasic.SqlErrorsCollection = New SqlErrorsCollection()
-        For index = 0 To args.Errors.Count - 1
-            sqlErrors.Add(New Databasic.Oracle.SqlError(args.Errors(index)))
-        Next
-        Databasic.Events.RaiseError(sqlErrors)
-    End Sub
+	Protected Shared Sub errorHandler(sender As Object, args As OracleInfoMessageEventArgs)
+		Dim sqlErrors As Databasic.SqlErrorsCollection = New SqlErrorsCollection()
+		For index = 0 To args.Errors.Count - 1
+			sqlErrors.Add(New Databasic.Oracle.SqlError(args.Errors(index)))
+		Next
+		Databasic.Events.RaiseError(sqlErrors)
+	End Sub
 
-    Protected Overrides Function createAndBeginTransaction(Optional transactionName As String = "", Optional isolationLevel As IsolationLevel = IsolationLevel.Unspecified) As Databasic.Transaction
+	Protected Overrides Function createAndBeginTransaction(Optional transactionName As String = "", Optional isolationLevel As IsolationLevel = IsolationLevel.Unspecified) As Databasic.Transaction
         Me.OpenedTransaction = New Transaction() With {
             .ConnectionWrapper = Me,
             .Instance = Me._provider.BeginTransaction(isolationLevel)
